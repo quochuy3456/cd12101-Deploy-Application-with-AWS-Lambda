1fe781c04ba180c903700f55336e6dd1a0809565
QuocHuyDo <quochuydo.cn@gmail.com>
2024-11-10 11:53:55 +0700

    Add config file into client

---
+++ starter/client/config.ts
@@ -0,0 +1,3 @@
+const REACT_APP_AUTH0_DOMAIN="dev-sqxm8d99.us.auth0.com"
+const REACT_APP_AUTH0_CLIENT_ID="OElPaF9GWyH7pWa564sNMyzHa47yviqN"
+const REACT_APP_API_ENDPOINT="https://sovj4x1vli.execute-api.us-east-1.amazonaws.com/dev"
